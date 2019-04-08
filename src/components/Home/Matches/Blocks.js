5bb241fe629a5dbc12599874a793afc15e4d7600
LeedWover <aresone.gaben@gmail.com>
2019-04-08 14:21:09 +0200

    homepage fetch data error and loading handling

--- src/components/Home/Matches/Blocks.js
+++ src/components/Home/Matches/Blocks.js
@@ -1,55 +1,67 @@
-import React, { Component } from 'react';
-import { Slide } from 'react-reveal';
+import React, { Component } from "react";
+import { Slide } from "react-reveal";
+import { CircularProgress } from "@material-ui/core";
 
-import { firebaseMatches } from '../../../firebase';
-import MatchBlock from '../../utils/MatchesBlock';
-import { firebaseLooper, /*reverseArray*/ } from '../../utils/Misc';
+import { firebaseMatches } from "../../../firebase";
+import MatchBlock from "../../utils/MatchesBlock";
+import { firebaseLooper /*reverseArray*/ } from "../../utils/Misc";
 
 class Blocks extends Component {
-  
   state = {
-    matches: []
-  }
-  
+    matches: [],
+    isLoading: true,
+    errorMessage: "",
+    error: false
+  };
+
   componentDidMount() {
-    firebaseMatches.limitToLast(6).once('value')
+    firebaseMatches
+      .limitToLast(6)
+      .once("value")
       .then(snapshot => {
         const matches = firebaseLooper(snapshot);
-        
+
         this.setState({
           //matches: reverseArray(matches)
-          matches: matches.reverse()
-        })
+          matches: matches.reverse(),
+          isLoading: false
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: true,
+          isLoading: false,
+          errorMessage: err.message
+        });
       });
   }
 
   render() {
-
+    console.log(this.state);
     let showMatches = this.state.matches.map(match => {
       return (
-        <Slide
-          bottom
-          key={match.id}
-        >
-          <div
-            className="item" 
-          >
+        <Slide bottom key={match.id}>
+          <div className="item">
             <div className="wrapper">
-              <MatchBlock
-                
-                match={match}
-              />
+              <MatchBlock match={match} />
             </div>
           </div>
         </Slide>
-      )
-    })
+      );
+    });
 
     return (
       <div className="home_matches">
-        {showMatches}
+        {this.state.isLoading ? (
+          <CircularProgress
+            thickness={7}
+            style={{ color: "#fff", margin: "auto", marginBottom: "3em" }}
+          />
+        ) : (
+          this.state.error ? <div style={{margin: 'auto', marginBottom: '3em', fontSize: '1.5em', color: 'red'}}>{this.state.errorMessage}</div> : showMatches
+        )}
       </div>
-    )
+    );
   }
 }
 
